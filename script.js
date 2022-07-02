7853b6d1b8272e34b7658bcf1a1cd7c921d83fe7
alielmala <ali.elmala22@gmail.com>
2022-07-03 00:41:07 +0200

    Finish

--- script.js
+++ script.js
@@ -4,14 +4,35 @@ let menu = document.getElementById("menu");
 
 closeNav.addEventListener("click",function(){
     navbar.classList.remove("expand-hr");
+    document.getElementById("features-list").classList.remove("expand-vr");
+    document.getElementById("companies-list").classList.remove("expand-vr");
 })
 menu.addEventListener("click", function(){
     navbar.classList.add("expand-hr");
 })
-
+let countFeat =0, countComp=0;
 document.getElementById("features").addEventListener("click",function(){
-    document.getElementById("features-list").classList.add("expand-vr");
+        if (countFeat%2==0){
+            document.getElementById("features-list").classList.add("expand-vr");
+            document.getElementById("feature-arrow").classList.add("rotate");
+            countFeat++;
+        }
+        else{
+            document.getElementById("features-list").classList.remove("expand-vr");
+            document.getElementById("feature-arrow").classList.remove("rotate");
+            countFeat++;
+        }
+        
 })
 document.getElementById("companies").addEventListener("click",function(){
-    document.getElementById("companies-list").classList.add("expand-vr");
+    if (countComp%2==0){
+        document.getElementById("companies-list").classList.add("expand-vr");
+        document.getElementById("company-arrow").classList.add("rotate");
+        countComp++;
+    }
+    else{
+        document.getElementById("companies-list").classList.remove("expand-vr");
+        document.getElementById("company-arrow").classList.remove("rotate");
+        countComp++;
+    }
 })
